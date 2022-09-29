70754ff97635a69c5edb7678c82a9f6c204938fe
RodrigoTopan <rodrigo.topan.ti@gmail.com>
2022-09-29 20:08:47 -0300

    chore: iniital commits

---
+++ src/main/kotlin/com/clean/architecture/cleancepsearcherapi/CleanCepSearcherApiApplication.kt
@@ -0,0 +1,11 @@
+package com.clean.architecture.cleancepsearcherapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanCepSearcherApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CleanCepSearcherApiApplication>(*args)
+}
